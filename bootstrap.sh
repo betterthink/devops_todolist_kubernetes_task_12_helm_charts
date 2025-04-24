bad6a6587103b0ecac2144bb931656dd25d251f0
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-04-24 10:33:28 +0300

    fix the issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create cluster --config cluster.yml
+kind create cluster --config cluster.yml
 
 kubectl taint nodes kind-worker app=mysql:NoSchedule
 kubectl taint nodes kind-worker2 app=mysql:NoSchedule
